29c96f4bed368b0d9a043e1ae6fd362f3dea491b
Haven Barnes <haven38@gmail.com>
2020-06-27 01:32:17 -0700

    Added request throttling and basic analysis. Needs two degree separation now

--- src/index.js
+++ src/index.js
@@ -2,19 +2,54 @@ import TwitterClient from './twitter-client.js';
 
 const client = new TwitterClient();
 
-const getNLeastFollowedFollowers = async (username, N) => {
-	const followers = await client.getFollowers(username);
-	const userPromises = followers.map(async (id) => {
-		const user = await client.getUser(id);
-		return user;
+const getNLeastFollowedFriends = async (friends, N, maxFollowers) => {
+	const userPromises = friends.map(async (id) => {
+		return await client.getUser(id);
 	});
-	const users = await Promise.all(userPromises);
-	console.log(users[0]);
+	let users = await Promise.all(userPromises);
+	users = users.sort((a, b) => a.followers - b.followers);
+	users = users.slice(0, N);
+	users = users.filter((u) => u.followers <= maxFollowers);
+	return users;
+};
+
+const getIntersectionSize = (set1, set2) => {
+	return new Set([...set1].filter((x) => set2.has(x))).size;
+};
+
+const getPotentialRankingByIntersection = (friendsSet, potentialWinners) => {
+	const friendsWithRankings = potentialWinners.map((potential) => {
+		return {
+			intersectionSize: getIntersectionSize(friendsSet, potential.friends),
+			...potential,
+		};
+	});
+	let ranked = friendsWithRankings.sort((a, b) => b.intersectionSize - a.intersectionSize);
+	return ranked.map((f) => f.username);
 };
 
 const main = async () => {
 	const username = process.env.TWITTER_USER;
-	const tenFollowers = await getNLeastFollowedFollowers(username, 10);
+
+	// Get everyone who <username> follows
+	const friends = await client.getFriends(username);
+	// Get the 10 least-followed people they follow
+	const specialFriends = await getNLeastFollowedFriends(friends, 25, 500);
+	// then grab nested special friends of these special friends, put them all in a big list together
+
+	// See which people the special frineds follow
+	const potentialWinners = await Promise.all(
+		specialFriends.map(async (user) => {
+			const userFriends = await client.getFriends(user.username);
+			return {
+				username: user.username,
+				friends: new Set(userFriends),
+			};
+		}),
+	);
+
+	const potentialsRanked = getPotentialRankingByIntersection(new Set(friends), potentialWinners);
+	console.log(potentialsRanked);
 };
 
 main();
--- src/twitter-client.js
+++ src/twitter-client.js
@@ -1,5 +1,6 @@
 import Twitter from 'twitter-lite';
 import cache from 'node-file-cache';
+import throttledQueue from 'throttled-queue';
 import twitterConfig from './config/twitter-config.js';
 import { formatTwitterUser } from './utils/twitter-utils.js';
 
@@ -12,6 +13,8 @@ export default class TwitterClient {
 	constructor() {
 		this.twit = new Twitter(twitterConfig);
 		this.cache = cache.create({ life: 1800 });
+		this.listThrottle = throttledQueue(1, 60000);
+		this.userThrottle = throttledQueue(1, 1000);
 	}
 
 	async getFollowers(username) {
@@ -30,17 +33,23 @@ export default class TwitterClient {
 		}
 	}
 
-	async getFollowing(username) {
+	async getFriends(username) {
 		const cacheKey = `${CACHE_KEY_PREFIXES.GET_FRIENDS_IDS}/${username}`;
 		if (!!this.cache.get(cacheKey)) {
 			return this.cache.get(cacheKey);
 		}
 
 		try {
-			const following = await this.twit.get('friends/ids', { screen_name: username, count: 5000, stringify_ids: true });
-			console.log(`Fetched ${following.ids.length} who follow ${username}`);
-			this.cache.set(cacheKey, followers.ids);
-			return following.ids;
+			const friends = await new Promise((resolve, reject) => {
+				this.listThrottle(async () => {
+					const result = await this.twit.get('friends/ids', { screen_name: username, count: 5000, stringify_ids: true });
+					resolve(result);
+				});
+			});
+
+			console.log(`Fetched ${friends.ids.length} who follow ${username}`);
+			this.cache.set(cacheKey, friends.ids);
+			return friends.ids;
 		} catch (error) {
 			console.log(error);
 		}
@@ -53,7 +62,12 @@ export default class TwitterClient {
 		}
 
 		try {
-			let user = await this.twit.get('users/show', { user_id: `${userId}` });
+			let user = await new Promise((resolve, reject) => {
+				this.userThrottle(async () => {
+					const result = await this.twit.get('users/show', { user_id: `${userId}` });
+					resolve(result);
+				});
+			});
 			user = formatTwitterUser(user);
 			this.cache.set(cacheKey, user);
 			return user;
